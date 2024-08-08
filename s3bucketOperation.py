9dda6a698b5b5e1650052f357592258296601dec
sridevib0408 <52267276+sridevib0408@users.noreply.github.com>
2024-08-08 22:32:16 +0530

    Update s3bucketOperation.py

--- s3bucketOperation.py
+++ s3bucketOperation.py
@@ -3,7 +3,7 @@ client = boto3.client('s3')
 
 response = client.create_bucket(
     ACL='private',
-    Bucket='javahomecloud123',
+    Bucket='MyBucketSrideviB',
     CreateBucketConfiguration={
         'LocationConstraint': 'ap-south-1'
     }
@@ -14,20 +14,20 @@ file_reader = open('create_bucket.py').read()
 response = client.put_object(
     ACL='private',
     Body=file_reader,
-    Bucket='javahomecloud123',
+    Bucket='MyBucketSrideviB',
     Key='create_bucket.py'
 )
 
 #Delete Object
 response = client.delete_object(
-    Bucket='javahomecloud123',
+    Bucket='MyBucketSrideviB',
     Key='create_bucket.py'
 )
 
 
 #list Objects
 response = client.list_objects(
-    Bucket='javahomecloud123'
+    Bucket='MyBucketSrideviB'
 )
 
 for content in response['Contents']:
@@ -37,3 +37,20 @@ for content in response['Contents']:
 response = client.list_buckets()
 for bucket in response['Buckets']:
     print(bucket['Name'])
+
+#get specific rows and colums from s3 bucket
+resp = client.select_object_content(
+    Bucket = 'MyBucketSrideviB',
+    Key = 'file/Myfile.csv',
+    Expression = 'select s.name,s.email from s3Object as s,
+    ExpressionType ='SQL',
+    InputSerialization = {CSV:{'FileHeaderInfo':'Use'}}
+    OutputSerialization = {CSV:{}}
+)
+#list the data from csv file
+for event in resp['payload']:
+    if 'Records' in event:
+        print(event['Records']['payload'].decode())
+    print(
+
+
